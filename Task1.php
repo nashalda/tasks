bb2987e816e82467b4659ce22362c94af07e85ea
Nick <snuker@tut.by>
2022-04-01 15:34:18 +0300

    Added solution

--- Task1.php
+++ Task1.php
@@ -4,8 +4,14 @@ namespace src;
 
 class Task1
 {
-    function main()
+    function main(int $digit): string
     {
-
+        return $digit > 30
+            ? 'More than 30'
+            : ($digit > 20
+                ? 'More than 20'
+                : ($digit > 10
+                    ? 'More than 10'
+                    : 'Less than 10'));
     }
 }
